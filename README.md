ea609112f28fa3864baac8542c2c5a83263ce656
Sricharan <kalikisrichsanreddy@gmail.com>
2024-03-25 00:19:20 -0400

    add looping and leveling functionality

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # Unlimita-RPG
+
+## Description
+
+This is my READ ME for the turn based game unlimita rpg. Having played many turn based rpg games I always imagined the workings behind one and how difficult it might be to make one. I wanted to make a simple game that took elements from many rpg adventure games which had turned based mechanics and remove the advenure part to focus on making it a pure turn based strategy game. The player battles with their arch nemesis the slime which gets stronger with each victory for the player. As you defeat enemeies [slimes are our only enemy at the moment] your highscore increases by the number of victories the you achieve. get a highscore by strategically investing points in to your player as you level up to create a character that can take down more powerful slimes.  
+
+## Technologies Used
+
+HTML,CSS,Javascript
+
+## Getting Started
+
+https://unlimita-7un7621lz-sricharans-projects-574a6f51.vercel.app
+
+you may choose to enter your name before starting the game. It will allow your username to be displayed when displaying highscore as you return to the title screen. It also allows the player name to be the same as your username. As of now no username info is being stored anywhere. 
+
+## wireFrames/screenShots
+
+![Title Screen](photos/TitleScreen.png)
+![battleScene](photos/battleScene.png)
+![levelingUpScene](photos/levelUpScene.png)
+## Timeline
+
+| **Day** | **Tasks** | **Goal completion** | **Notes/ Thoughts** |
+| --- | --- | --- | --- |
+| 3/18 | Define constants classes,functions and event listeners |  Y   |  added html elements and classes and stuff   |
+| 3/19 | Improve functioning and get a running prototype |  N   |  had issues with implementing turn orders  |
+| 3/20 | work on adding css to the game and improve visibility|  Y   |  added grids  and backgrounds  |
+| 3/21 | get feedback from instructors on necessary changes |   Y  | got help and feedback from instructors  |
+| 3/22 | work on adding endless mode and highscore as well as enchancement screen |   N  | changed up this plan slightly    |
+| 3/23 | add sprites animations sfx and other stretch goals|   N  |  did not have time to implements animations and sfx   |
+| 3/24 | review code get feedback and have others test it and make improvements/check for bugs|  Y   |  fixed functionality  problems |
+
+## Attributions
+
+The background for the battle scene: https://64.media.tumblr.com/00ec803404f6e9d6583f92d8870b5fb8/tumblr_p7k8f3fMWS1wvbydeo1_1280.png
+The background for the start screen: https://pbs.twimg.com/media/GHmHx-bWgAAmMgB.png:large
+
+## Next Steps
+
+-I plan on working to implement different enemy sprites, different enemies and enemy attacks
+-I plan on working to add different battle scenes
+-I plan on working to use sound effects for attacks
+-I plan on working to make different player moves that the player can slot in
+-I plan on adding save progress functionality 
--- script.js
+++ script.js
@@ -1,30 +1,5 @@
-//Define constants to get the elements of start page in javascript add classes to the elements
-//Define constants to get the elements of the battle screen in javascript
-//Define constants to get the elements of the enhancement screen
-//Add classes to each elements in the start page using javascript
-//Define classes for players,equipment and enemies
-//add a fuction to render elements of the start page 
-//add a function to render the battle screen to make enemies and player get displayed
-//add a function to decide which player starts first then call a function to calculate damage dealt
-//add a function to calculate the damage done by the first character
-//add a function to calculate the damage done by the enemy 
-//add a function to check for deathblows to enemies and call a function to render the enhancements screen
-//add a function to check for deathblows to the player and call a function to render the start screen also updating the highscore 
-//add a function to change equipment
-//add a function to augment equipment stats on to player stats 
-/*rendered start menu must have 
-.Game title 
-.username input field
-.highscore list
-.start button
-*/
-//add an event listener to the input field to update the username
-//add an event listener to the start button in the start menu
-//add an event listener to the each selection of the attack menu
-//add an event listener to see equipment stats on hover   
-//add an event listener to make equipment to choose between equipment or experience
 
-// 1. Declare the start page elements
+// Declare the start page elements
 
 const page=document.querySelector('body')
 const titleScreen=document.querySelector('.title-screen')
@@ -37,20 +12,23 @@ const input=document.createElement('input')
 input.type='text'
 input.placeholder='type a name and hit enter'
 input.classList.add('user-name-input')
-const highScore=document.createElement('p')
 const highScoreList=document.createElement('ul')
-highScore.appendChild(highScoreList)
+highScoreList.classList.add('high-score-list')
+const highScoreLi=document.createElement('li')
+highScoreList.appendChild(highScoreLi)
 const userName=document.createElement('p')
 userName.classList.add('user-name')
 const startButton=document.createElement('button')
+startButton.classList.add('start-button')
 startButton.type='button'  
 startButton.innerHTML="Start Game"
 const textBox=document.createElement('p')
 textBox.classList.add('text-box')
 
-// 2.Declare the battle page elements
+//Declare the battle page elements
 
 //player elements
+
 const playerName=document.createElement('p')
 playerName.classList.add('player-name')
 playerName.innerHTML='player1'
@@ -74,6 +52,7 @@ const playersRightLeg=document.createElement('div')
 const playersLeftLeg=document.createElement('div')
 
 //enemy elements
+
 const enemyName=document.createElement('p')
 enemyName.classList.add('enemy-name')
 const enemyHealthBar=document.createElement('p')
@@ -93,6 +72,7 @@ const enemyLeftEye=document.createElement('div')
 const enemyRightEye=document.createElement('div')
 
 //page elements
+
 const battleScreen=document.createElement('div')
 battleScreen.classList.add('battle-screen')
 const attackBox=document.createElement('div')
@@ -105,21 +85,57 @@ const attackFourName=document.createElement('li')
 const battleInfoBox=document.createElement('p')
 battleInfoBox.classList.add('battle-info-box')
 const attacksListArray=[attackOneName,attackTwoName,attackThreeName,attackFourName]
+
+//level up screen elements
+const levelUpScreen=document.createElement('div')
+levelUpScreen.classList.add('level-up-screen')
+const totalPoints=document.createElement('p')
+totalPoints.classList.add('total-points')
+totalPoints.innerHTML='Total Points'
+const totalPointsNum=document.createElement('p')
+totalPointsNum.classList.add('total-points-number')
+let totalPointsNumber
+const attackPoints=document.createElement('p')
+attackPoints.classList.add('attack-points')
+attackPoints.innerHTML='Attack Points'
+const attackPointsNum=document.createElement('p')
+attackPointsNum.classList.add('attack-points-number')
+let attackPointsNumber
+const healthPoints=document.createElement('p')
+healthPoints.classList.add('health-points')
+healthPoints.innerHTML='Health Points'
+const healthPointsNum=document.createElement('p')
+healthPointsNum.classList.add('health-points-number')
+let healthPointsNumber
+const speedPoints=document.createElement('p')
+speedPoints.classList.add('speed-points')
+speedPoints.innerHTML='Speed Points'
+const speedPointsNum=document.createElement('p')
+speedPointsNum.classList.add('speed-points-number')
+let speedPointsNumber
+const distributePoints=document.createElement('button')
+distributePoints.classList.add('distribute-points-button')
+distributePoints.innerHTML='Enter' 
+
+const attackUpButton=document.createElement('button')
+attackUpButton.classList.add('attack-up-button')
+const attackDownButton=document.createElement('button')
+attackDownButton.classList.add('attack-down-button')
+const speedUpButton=document.createElement('button')
+speedUpButton.classList.add('speed-up-button')
+const speedDownButton=document.createElement('button')
+speedDownButton.classList.add('speed-down-button')
+const healthUpButton=document.createElement('button')
+healthUpButton.classList.add('health-up-button')
+const healthDownButton=document.createElement('button')
+healthDownButton.classList.add('health-down-button')
 const leaveBattle=document.createElement('button')
 leaveBattle.classList.add('leave-battle-button')
 leaveBattle.innerHTML='return to title screen'
-leaveBattle.addEventListener('click',function(){ 
-    playerCharacter.removeChild(playerBody)
-    playerCharacter.removeChild(playerLegs)
-    enemyCharacter.removeChild(enemyEyes)
-    enemyCharacter.removeChild(enemyFace)
-    enemyCharacter.removeChild(enemyMouth)
-    battleScreen.removeChild(enemyCharacter)
-    battleScreen.removeChild(playerCharacter)
-    page.removeChild(battleScreen)
-    titleScreenPage()
-})
-// 3. declare the necessary classes
+leaveBattle.addEventListener('click',returnToTitleScreen)
+
+
+// declare the necessary classes
 
 
 class Player{
@@ -181,18 +197,30 @@ class attack{
     }   
 }
 
-// 4. make a new player from the class
+// make a new player from the class
 
-const player1=new Player(1,20,20,10,10)
+const player1=new Player(1,20,20,10,11)
 player1.name='player1'
+let currentAttack=player1.attack
+let currentMaxHealth=player1.maxHealth
+let currentSpeed=player1.speed
 
-// 5. function for the start page
+// 
+
+function returnToTitleScreen(){
+    battleScreen.removeChild(enemyCharacter)
+    battleScreen.removeChild(playerCharacter)
+    page.removeChild(battleScreen)
+    titleScreenPage()
+    titleScreen.appendChild(highScoreList)
+    highScoreLi.innerHTML= `HighScore: ${player1.level}` 
+}
+//  function for the start page
 
 function titleScreenPage(){
     page.appendChild(titleScreen)
     titleScreen.appendChild(gameTitle)
     gameTitle.innerHTML='Unlimita RPG'
-    titleScreen.appendChild(highScore)
     titleScreen.appendChild(startButton)
     titleScreen.appendChild(userNamePrompt)
 }
@@ -232,7 +260,7 @@ function playerSprite(){
     playersLeftLeg.classList.add('players-left-leg')
 }
 
-// 6.function for the battle page 
+// function for the battle page 
 
 function battleScreenPage(){
     page.appendChild(battleScreen)
@@ -263,6 +291,29 @@ userNamePrompt.addEventListener('click',function(){
    titleScreen.appendChild(input)
 })
  
+//function for the level up screen
+
+function displayLevelUp(){
+
+battleScreen.appendChild(levelUpScreen)
+levelUpScreen.appendChild(totalPoints)
+levelUpScreen.appendChild(totalPointsNum)
+levelUpScreen.appendChild(attackPoints)
+levelUpScreen.appendChild(attackPointsNum)
+levelUpScreen.appendChild(healthPoints)
+levelUpScreen.appendChild(healthPointsNum)
+levelUpScreen.appendChild(speedPoints)
+levelUpScreen.appendChild(speedPointsNum)
+levelUpScreen.appendChild(attackUpButton)
+levelUpScreen.appendChild(attackDownButton)
+levelUpScreen.appendChild(healthUpButton)
+levelUpScreen.appendChild(healthDownButton)
+levelUpScreen.appendChild(speedUpButton)
+levelUpScreen.appendChild(speedDownButton)
+levelUpScreen.appendChild(distributePoints)
+}
+
+
 input.addEventListener('keydown',function(event){ 
     if(event.key==='Enter')
     {
@@ -272,21 +323,107 @@ input.addEventListener('keydown',function(event){
     }
 })
 
-const fireball= new attack('fireball',3,0,0,0,0)
+distributePoints.addEventListener('click',function(){
+    if(totalPointsNumber==0){
+    player1.attack+=attackPointsNumber
+    player1.maxHealth+=healthPointsNumber
+    player1.speed+=speedPointsNumber
+    player1.level+=1
+    enemy1.level+=1
+    enemy1.attack=Math.floor((enemy1.attack)*1.1)
+    enemy1.maxHealth=Math.floor((enemy1.maxHealth*1.1))
+    enemy1.speed=Math.floor((enemy1.speed*1.1))
+    enemy1.currentHealth=enemy1.maxHealth
+    levelUpScreen.classList.add('hide-level-up-screen')
+    battleInfoBox.innerHTML='congrats you leveled up'
+    attackList.classList.remove('hide-attack-list')
+    battleScreen.appendChild(enemyCharacter)
+    battleScreen.appendChild(enemyCharacterinfo)
+    battleScreen.appendChild(enemyName)
+    battleScreen.appendChild(enemyLevel)
+    battleScreen.appendChild(enemyHealthRatio)
+    createEnemy()
+    createPlayer()
+    }
+})
+
+attackUpButton.addEventListener('click',function(){
+    if(totalPointsNumber>0){
+    totalPointsNumber-=1;
+    attackPointsNumber+=1;
+    }
+    updateLevelUpScreen()
+})
+attackDownButton.addEventListener('click',function(){
+    if(totalPointsNumber<6 && attackPointsNumber>0){
+    totalPointsNumber+=1;
+    attackPointsNumber-=1;
+    }
+    updateLevelUpScreen()
+})
+healthUpButton.addEventListener('click',function(){
+    if(totalPointsNumber>0){
+    totalPointsNumber-=1;
+    healthPointsNumber+=1;
+    }
+    updateLevelUpScreen()
+})
+healthDownButton.addEventListener('click',function(){
+    if(totalPointsNumber<6 && healthPointsNumber>0){
+    totalPointsNumber+=1;
+    healthPointsNumber-=1;
+    }
+    updateLevelUpScreen()
+})
+
+speedUpButton.addEventListener('click',function(){
+    if(totalPointsNumber>0){
+    totalPointsNumber-=1;
+    speedPointsNumber+=1;
+    }
+    updateLevelUpScreen()
+})
+speedDownButton.addEventListener('click',function(){
+    if(totalPointsNumber<6 && speedPointsNumber>0){
+    totalPointsNumber+=1;
+    speedPointsNumber-=1;
+    }
+    updateLevelUpScreen()
+})
+
+function updateLevelUpScreen(){
+    totalPointsNum.innerHTML=`${totalPointsNumber}`
+    attackPointsNum.innerHTML=`${attackPointsNumber}`
+    healthPointsNum.innerHTML=`${healthPointsNumber}`
+    speedPointsNum.innerHTML=`${speedPointsNumber}`
+}
+
+const fireball= new attack('fireball',30,0,0,0,0)
 const heal=new attack('heal',0,5,0,0,0)
 const agility=new attack('agility',0,0,0,0,5)
 const reckless=new attack('reckless',0,0,3,8,5)
 const attacksArray=[fireball,heal,agility,reckless]
 
 
-function damageToEnemyCalc(attack,damage){
-    let damageDealt=Math.floor((attack/10)*damage);
+function damageToEnemyCalc(currentAttack,damage){
+    let damageDealt=Math.floor((currentAttack/10)*damage)
     enemy1.currentHealth= enemy1.currentHealth-damageDealt
     if(enemy1.currentHealth<=0)
     {
         battleInfoBox.removeEventListener('click',referenceNextTurn)
         enemy1.currentHealth = 0
         createEnemy()
+        battleScreen.removeChild(enemyCharacter)
+        battleScreen.removeChild(enemyName)
+        battleScreen.removeChild(enemyLevel)
+        battleScreen.removeChild(enemyHealthRatio)
+        battleScreen.removeChild(enemyCharacterinfo)
+        totalPointsNumber=5
+        attackPointsNumber=0
+        healthPointsNumber=0
+        speedPointsNumber=0
+        updateLevelUpScreen()
+        displayLevelUp()
         attackList.classList.add('hide-attack-list')
         battleInfoBox.innerHTML=`${player1.name} has defeated the ${enemy1.name}.`
         return damageDealt
@@ -294,8 +431,8 @@ function damageToEnemyCalc(attack,damage){
     createEnemy()
     battleInfoBox.innerHTML+=`${player1.name} did ${damageDealt} damage to the ${enemy1.name}.`
 }    
-function damageToPlayerCalc(attack,damage){
-    let damageDealt=Math.floor((attack/10)*damage)
+function damageToPlayerCalc(currentAttack,damage){
+    let damageDealt=Math.floor((currentAttack/10)*damage)
     player1.currentHealth= player1.currentHealth-damageDealt
     if(player1.currentHealth<=0)
     {
@@ -303,7 +440,7 @@ function damageToPlayerCalc(attack,damage){
         player1.currentHealth = 0
         createPlayer()
         attackList.classList.add('hide-attack-list')
-        battleInfoBox.innerHTML=`${player1.name} has defeated the ${enemy1.name}`
+        battleInfoBox.innerHTML=`${player1.name} has been defeated click return to title screen`
         return damageDealt
     }
     createPlayer()
@@ -311,8 +448,8 @@ function damageToPlayerCalc(attack,damage){
 }   
 function increasePlayerHealth(selfHealthInc){
     player1.currentHealth=player1.currentHealth+selfHealthInc
-    if(player1.currentHealth>player1.maxHealth)
-    player1.currentHealth=player1.maxHealth
+    if(player1.currentHealth>currentMaxHealth)
+    player1.currentHealth=currentMaxHealth
     createPlayer()
     battleInfoBox.innerHTML+=`${player1.name} healed themselves by ${selfHealthInc} points.`
 }
@@ -330,12 +467,12 @@ function decreasePlayerHealth(selfHealthDec){
     battleInfoBox.innerHTML+=`${player1.name} hurt themselves, took ${selfHealthDec} damage.`
 }
 function  increasePlayerAttack(incAtk){
-    player1.attack=player1.attack+incAtk
+    currentAttack=currentAttack+incAtk
     createPlayer()
     battleInfoBox.innerHTML+=`${player1.name}  raised their by attack ${incAtk}.`  
 }
 function  increasePlayerSpeed(incSpd){
-    player1.speed=player1.speed+incSpd
+    currentSpeed=currentSpeed+incSpd
     createPlayer()
     battleInfoBox.innerHTML+=`${player1.name}'s speed increased by ${incSpd}.`  
 }
@@ -372,7 +509,7 @@ const referenceNextTurn=function(baseDmg,selfHealthInc,selfHealthDec,incAtk,incS
          nextTurn(baseDmg,selfHealthInc,selfHealthDec,incAtk,incSpd)
 }
 function speedCheck(){
-    if(player1.speed>enemy1.speed)
+    if(currentSpeed>enemy1.speed )
     {
         player1.turn=true
     }
@@ -387,7 +524,7 @@ function speedCheck(){
 
 function playerActions(baseDmg,selfHealthInc,selfHealthDec,incAtk,incSpd){
     if(baseDmg!==0){
-        damageToEnemyCalc(player1.attack,baseDmg)
+        damageToEnemyCalc(currentAttack,baseDmg)
       }  
       if(selfHealthInc!==0){
         increasePlayerHealth(selfHealthInc)
@@ -462,7 +599,7 @@ function makeAttackList(){
             })
         }
      }
-const enemy1= new enemy('slime',1,13,13,10,9)
+const enemy1= new enemy('slime',1,10,10,10,10)
 const bite= new attack('bite',2,0,0,0,0)
 const leech=new attack('leech',3,1,0,0,0)
 enemy1.attacks=[bite,leech]
